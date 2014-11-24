03ea2fb9ee7a7e83c6e45560df87ba708d4c3562
asmacdo <asmacdo@gmail.com>
2014-11-24 10:09:54 -0500

    working CLI version

--- pr8.py
+++ pr8.py
@@ -1,14 +1,26 @@
+#!/usr/bin/env python
+
 from github import Github
 import flake8.main
 import requests
+import sys
+
+try:
+    repo_name = sys.argv[1]
+    pull_number = int(sys.argv[2])
+except IndexError:
+    print("<user/repo>, <pr number (int)>")
 
 g = Github()
-repo = g.get_repo('pulp/pulp')
+repo = g.get_repo(repo_name)
+print "Got repo"
 
-
-pr = repo.get_pull(1355)
+pr = repo.get_pull(pull_number)
+print "got pr"
 for fp in pr.get_files():
+    print "working with file{a}".format(a=fp.filename)
     resp = requests.get(fp.raw_url)
     flake8.main.check_code(resp.content)
 
+sys.exit(1)
 # Next filter out all lines that are not "additions"
