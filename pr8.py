3ef51acb58d531be613d3dd30e65dd778cec26dc
asmacdo <asmacdo@gmail.com>
2014-12-01 09:20:57 -0500

    minor chanages

--- pr8.py
+++ pr8.py
@@ -18,7 +18,7 @@ print "Got repo"
 pr = repo.get_pull(pull_number)
 print "got pr"
 for fp in pr.get_files():
-    print "working with file{a}".format(a=fp.filename)
+    print "\n\n\nworking with file{a}".format(a=fp.filename)
     resp = requests.get(fp.raw_url)
     flake8.main.check_code(resp.content)
 
